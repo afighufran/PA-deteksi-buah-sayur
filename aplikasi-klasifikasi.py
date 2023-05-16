4494b224165b048b73fe4e227c397287d879b3e9
afighufran <leticiahd132@gmail.com>
2023-05-16 14:52:29 +0700

    Update aplikasi-klasifikasi.py

--- aplikasi-klasifikasi.py
+++ aplikasi-klasifikasi.py
@@ -49,8 +49,8 @@ def processed_img(img_path):
 
 
 def run():
-    st.title("Fruits🍍-Vegetable🍅 Classification")
-    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    st.title("Klasifikasi Buah dan Sayuran Beserta Kalori")
+    img_file = st.file_uploader("Pilih Gambar", type=["jpg", "png"])
     if img_file is not None:
         img = Image.open(img_file).resize((250, 250))
         st.image(img, use_column_width=False)
@@ -63,13 +63,13 @@ def run():
             result = processed_img(save_image_path)
             print(result)
             if result in vegetables:
-                st.info('**Category : Vegetables**')
+                st.info('**Kategori : Sayur**')
             else:
-                st.info('**Category : Fruit**')
-            st.success("**Predicted : " + result + '**')
+                st.info('**Kategori : Buah**')
+            st.success("**Hasil Prediksi : " + result + '**')
             cal = fetch_calories(result)
             if cal:
-                st.warning('**' + cal + '(100 grams)**')
+                st.warning('**' + cal + '(per 100 gram)**')
 
 
 run()
